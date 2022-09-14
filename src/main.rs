2df016bcd89e2a5e77f0b88983178c16dd6465bc
Bjarke Sporring <work.bjarke@gmail.com>
2022-09-14 10:14:16 +0200

    Add all 3 and 4bit color support. And use clap for cli interfacing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, BufRead};
 
+use clap::Parser;
+
 fn read_stdin() -> Result<String, io::Error> {
     let mut buffer: String = String::new();
     let stdin = io::stdin();
@@ -11,41 +13,73 @@ fn read_stdin() -> Result<String, io::Error> {
 }
 
 mod ansi {
-    pub fn color(color: &str) -> &str {
+    pub fn color(color: &str) -> i32 {
+        // 3-bit and 4-bit mode. Super simple. https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
         match color {
-            "white" => "37",
-            "yellow" => "33",
-            "red" => "31",
-            _ => "",
+            "brightwhite" => 97,
+            "brightcyan" => 96,
+            "brightmagenta" => 95,
+            "brightblue" => 94,
+            "brightyellow" => 93,
+            "brightgreen" => 92,
+            "brightred" => 91,
+            "brightblack" => 90,
+            "white" => 37,
+            "cyen" => 36,
+            "magenta" => 35,
+            "blue" => 34,
+            "yellow" => 33,
+            "green" => 32,
+            "red" => 31,
+            "black" => 30,
+            _ => 37,
         }
     }
 
-    pub fn style(color: &str) -> &str {
+    pub fn bg_color(c: &str) -> i32 {
+        return color(c) + 10;
+    }
+
+    pub fn style(color: &str) -> i32 {
         match color {
-            "bold" => "1",
-            _ => "",
+            "bold" => 1,
+            _ => 0,
         }
     }
 }
 
+fn wrap_ansi(input: String, style: &str, color: &str, is_bg: bool) -> String {
+    let s = ansi::style(style);
+    let c = ansi::color(color);
 
-fn wrap_ansi(input: String, style: &str, color: &str) -> String {
-    return format!(
-        "\u{001B}[{};{}m{}\u{001B}[0m",
-        ansi::style(style),
-        ansi::color(color),
-        input
-    );
+    return format!("\u{001B}[{};{}m{}\u{001B}[0m", s, c, input);
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    color: String,
+
+    #[clap(short = 's', long = "--style")]
+    style: String,
+
+    #[clap(short = 'b', long = "--bg", default_value_t = false)]
+    is_bg: bool,
 }
 
 fn main() -> io::Result<()> {
     let buffer = read_stdin();
-    let args: Vec<String> = std::env::args().collect();
+    let args = Args::parse();
 
-    let style = &args[1];
-    let color = &args[2];
-
-    print!("{}", wrap_ansi(buffer.unwrap(), style, color));
+    print!(
+        "{}",
+        wrap_ansi(
+            buffer.unwrap(),
+            args.style.as_str(),
+            args.color.as_str(),
+            args.is_bg
+        )
+    );
 
     Ok(())
 }
